af85f302842a5e997501e57b91846aefc9ecc8f3
mEEtGandhi <37804920+mEEt126@users.noreply.github.com>
2020-04-26 00:19:45 +0530

    Add files via upload

---
+++ RTC_CODE_DS1307_.ino
@@ -0,0 +1,39 @@
+
+#include <Wire.h>
+#include <DS1307.h>
+
+DS1307 clock;
+RTCDateTime dt;
+
+void setup()
+{
+  Serial.begin(38400);
+
+  // Initialize DS1307
+  Serial.println("Initialize DS1307");;
+  clock.begin();
+
+  // If date not set
+  if (!clock.isReady())
+  {
+    // Set sketch compiling time
+    clock.setDateTime(__DATE__, __TIME__);
+  }
+}
+
+void loop()
+{
+  dt = clock.getDateTime();
+
+  // For leading zero look to DS1307_dateformat example
+
+  Serial.print("MISSION TIME: ");
+  Serial.print(dt.year);   Serial.print("-");
+  Serial.print(dt.month);  Serial.print("-");
+  Serial.print(dt.day);    Serial.print(" ");
+  Serial.print(dt.hour);   Serial.print(":");
+  Serial.print(dt.minute); Serial.print(":");
+  Serial.print(dt.second); Serial.println("");
+
+  delay(1000);
+}
